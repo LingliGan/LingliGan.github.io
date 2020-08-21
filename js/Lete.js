92e48c29decdc2be02e1f47a9c29914d98deaa6a
liuliu03 <897992093@qq.com>
2020-08-21 14:34:13 +0800

    Site updated: 2020-08-21 14:34:13

---
+++ js/Lete.js
@@ -0,0 +1,7 @@
+// 设置页脚博主会动的心
+$(document).ready(function(e){
+    $('.copyright').html('©2020 <i style="color:#FF6A6A;animation: announ_animation 0.8s linear infinite;" class="fa fa-heartbeat"></i> Levin-lee');
+})
+// 移除页脚样式
+var app = document.getElementById('footer');
+app.removeAttribute('id')
